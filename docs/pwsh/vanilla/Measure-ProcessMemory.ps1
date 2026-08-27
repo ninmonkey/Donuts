4131c02522a5f4c8efb2593064fe088b82db8161
Jake Bolton <ninmonkeys@gmail.com>
2026-08-27 16:47:04 -0500

    add: `Measure-ProcessMemory`

---
+++ docs/pwsh/vanilla/Measure-ProcessMemory.ps1
@@ -0,0 +1,31 @@
+﻿#requires -PSEdition Core
+
+function Measure-ProcessMemory {
+    <#
+    .synopsis
+        Get Process memory usage
+    #>
+    param(
+        # exact process name
+        [string] $ExactName,
+
+        # else by regex patterns
+        [string] $RegexName
+    )
+
+    if( $ExactName ) {
+        $ps = Get-Process -Name $ExactName
+    } else {
+        $ps = Get-Process | ? Name -match $RegexName
+    }
+    $propNames64 = @( $ps )[0].psobject.Properties | ? name -Match '64' | % name | Sort-object
+
+    @( foreach( $prop in $propNames64 ) {
+        $bytes = ( $ps | Measure-Object -Property $prop -Sum).Sum
+        [pscustomobject]@{ Prop = $Prop;
+            Gb = $bytes / 1gb | Join-string -f '{0:n1}'
+            Mb = $bytes / 1mb | Join-String -f '{0:n1}'
+            Bytes = $Bytes
+        }
+    }) | Sort-Object gb -Descending
+}
