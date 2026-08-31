cc7b63bdaf6902c35e6c905ebd517cd9a52020d6
Jake Bolton <ninmonkeys@gmail.com>
2026-08-31 10:06:20 -0500

    add: buffer git foreach and tags

---
+++ buffer_inbox/git-foreach-ref.ps1
@@ -0,0 +1,26 @@
+﻿<#
+    file:///C:/Program%20Files/Git/mingw64/share/doc/git-doc/git-for-each-ref.html
+
+
+#>
+
+# [Ex 1]: each
+git for-each-ref --count=10 --sort='-*authordate'
+
+# [Ex 2]: each multi lien format
+$argFmt = @'
+--format='From: %(*authorname) %(*authoremail)
+Subject: %(*subject)
+Date: %(*authordate)
+Ref: %(*refname)
+
+%(*body)
+'@
+git for-each-ref --count=10 --sort='-*authordate' @argFmt 'refs/tags'
+
+
+# [Ex 3]: each on: 'refs/heads'
+git for-each-ref --shell --format="ref=%(refname)" 'refs/heads'
+
+# [Ex 4]: each on: 'refs/tags'
+git for-each-ref --shell --format="ref=%(refname)" 'refs/tags'
---
+++ buffer_inbox/git-tag-latest-sorted.ps1
@@ -0,0 +1,16 @@
+﻿
+# prefix "-" sets descending
+
+
+# by semantic versioning ( ex: 1.2.0, 1.10.0 )
+git tag --sort=-v:refname
+
+# by alphabetical
+git tag --sort=-refname
+
+# by creation time
+git tag --sort=-creatordate
+
+# Alternative: Closest Tag to Current CommitIf by "newest" you meant the most recent tag created in the commit history of your active branch,
+# you should use git describe instead
+git describe --tags --abbrev=0
